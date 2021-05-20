25d810de28c7a032ed7b30296bd513044cd5a474
Hari <harishkumarre.chiye@hcl.com>
2021-05-20 14:51:11 +0530

    initial commit

---
+++ driver-service/src/test/java/com/hcl/foodorder/driver/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.foodorder.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
