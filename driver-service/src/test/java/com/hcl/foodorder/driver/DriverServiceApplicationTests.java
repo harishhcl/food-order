03ba8c726f750eb592aa51ecb84fd8ed86210d63
Hari <harishkumarre.chiye@hcl.com>
2021-06-03 07:55:05 +0530

    adding Dockerfile to services

--- driver-service/src/test/java/com/hcl/foodorder/driver/DriverServiceApplicationTests.java
+++ driver-service/src/test/java/com/hcl/foodorder/driver/DriverServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.hcl.foodorder.driver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DriverServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
